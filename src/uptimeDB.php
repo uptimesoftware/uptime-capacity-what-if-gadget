6dba602c11e25a88063537fca0104db318b7a695
Arpit Chaudhari <arpitkumar@monocept.com>
2020-04-21 10:42:58 +0530

    Oracle Connection String Updated for Linux

--- src/uptimeDB.php
+++ src/uptimeDB.php
@@ -114,12 +114,12 @@ class uptimeDB
 		}
 		elseif ($this->dbType == "oracle")
 		{
-			//For Linux
+			//For Windows
 			//$this->DB = odbc_connect($this->ORACLE_ODBC_DRIVER, $this->dbUsername, $this->dbPassword);
 			
-			//For Windows
-			//$this->DB = odbc_connect("Driver=" . $this->ORACLE_ODBC_DRIVER . ";SERVER=" . $this->dbHost . ";DATABASE=" . $this->dbName ,$this->dbUsername, $this->dbPassword);
-
+			//For Linux
+			//$this->DB = odbc_connect("Driver=" . $this->ORACLE_ODBC_DRIVER . ";dbq=" . $this->dbHost . ":" . $this->dbPort . "/" . $this->dbName,$this->dbUsername, $this->dbPassword);
+			
 			if (!$this->DB)
 			{
 				printf("ODBC Connection Failed: " . odbc_errormsg());
